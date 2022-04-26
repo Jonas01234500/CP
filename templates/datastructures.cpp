2f17611186282139cdc876b947f34e2a724d7833
Jonas <jonas.dalchow@student.kit.edu>
2022-04-26 19:06:14 +0200

    add some comments and mo's algorithm

--- templates/datastructures.cpp
+++ templates/datastructures.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 /**
  * Union find with path compression, no union by rank.
+ * find and union in O(almost 1).
  */
 struct UnionFind {
     vector<int> parent;
-    //initialize with size
+    // initialize with size
     UnionFind(int n) {
         parent.resize(n);
         for (int i = 0; i < n; ++i) {
             parent[i] = i;
         }
     }
-    //find parent
+    // find parent
     int find(int u) {
         if (parent[u] == u) {
             return u;
@@ -22,7 +23,7 @@ struct UnionFind {
         parent[u] = find(parent[u]);
         return parent[u];
     }
-    //union nodes
+    // union nodes
     void unionf(int a, int b) {
         a = find(a);
         b = find(b);
@@ -31,30 +32,30 @@ struct UnionFind {
 };
 
 /**
- * Binary Indexed Tree or Fenwick Tree. Kind a useless.
- *
+ * Binary Indexed Tree or Fenwick Tree. Kinda useless, use segment tree instead.
+ * Update value and query interval in O(log n).
  */
 struct BinaryIndexedTree {
     vector<int> bit;
     int n;
-    //0 filled tree
+    // 0 filled tree
     BinaryIndexedTree(int n){
         this->n = n;
         bit.resize(n, 0); //maybe not fill with zeros
     }
-    //fill with start vector
+    // fill with start vector
     BinaryIndexedTree(vector<int> a) : BinaryIndexedTree(a.size()){
         for (int i = 0; i < a.size(); ++i) {
             update(i, a[i]);
         }
     }
-    //To be implemented. Should be reversible
+    // To be implemented. Should be reversible
     int func(int a, int b){
         return a+b;
     }
     // calculate func from r to 0
-    int calc(int r){
-        int ret = 0;//maybe different start value
+    int query(int r){
+        int ret = 0;// maybe different start value
         while (r>= 0){
             ret = func(ret, bit[r]);
             r = (r & (r + 1)) -1;
@@ -74,4 +75,65 @@ struct BinaryIndexedTree {
         update(r+1, -delta);
     }
 };
+
+
+/**
+ * Mo's algorithm. Answer offline queries in O(n * sqrt(n))
+ * Might need to implement data structure very efficiently.
+ */
+int blocksize; // set blocksize, optimally to sqrt(n)
+
+struct Query {
+    int l,r,idx;
+    bool operator<(Query other) const {
+        return make_pair(l/blocksize, r) < make_pair(other.l / blocksize , other.r);
+    }
+};
+
+struct Mo {
+    vector<int> elements;
+    int value;
+    // declare data structure
+    Mo(vector<int> elements){
+        this->elements = elements;
+        value = 0;
+        // initialize structure
+    }
+    void remove(int idx){
+        // implement remove
+    }
+    void add(int idx){
+        // implement add
+    }
+    int getAnswer(){
+        // implement get answer
+    }
+    vector<int> algorithm(vector<Query> queries) {
+        vector<int> answers(queries.size());
+        std::sort(queries.begin(), queries.end());
+
+        int curL = 0, curR = -1;
+        for(Query q : queries){
+            while (curL > q.l){
+                curL--;
+                add(curL);
+            }
+            while (curR < q.r){
+                curR++;
+                add(curR);
+            }
+            while (curL < q.l){
+                remove(curL);
+                curL++;
+            }
+            while (curR > q.r){
+                remove(curR);
+                curR--;
+            }
+            answers[q.idx] = getAnswer();
+        }
+        return answers;
+    }
+};
+
 
