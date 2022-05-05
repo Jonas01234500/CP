67f0f6a013d9650560bf2fbeda62f9e8195a1f12
Jonas <jonas.dalchow@student.kit.edu>
2022-05-05 16:20:46 +0200

    add start template

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+#define double long double
+#define ii pair<int, int>
+#define ui uint64_t
+#define nl "\n"
+#define yes "YES\n"
+#define no "NO\n"
+#define inv vector<int> v(n);for(int i = 0; i<(n);++i){cin >> v[i];}
+#define st ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define fi first
+#define si second
+#define tsolve() int t; cin >> t;while(t--){solve();}
+#define print(b) if(b){cout << "YES\n";}else {cout << "NO\n";}
+
+int mo = 1020202009;
+
+struct sat {
+
+};
+
+void solve() {
+
+}
+
+int32_t main() {
+    st;
+    solve();
+}
