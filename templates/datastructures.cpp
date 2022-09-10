4a5d1995b58594625b6ede02696f691d571599a3
Jonas <jonas.dalchow@student.kit.edu>
2022-09-10 02:46:39 +0200

    add treap

--- templates/datastructures.cpp
+++ templates/datastructures.cpp
@@ -252,4 +252,46 @@ struct LazyTree{
 
 };
 
+/** Implizit Treap, split and merge in O(log n).
+ *
+ */
+struct item {
+    int val, prior, cnt;
+    item *l, *r;
+    item () { }
+    item (int val) : val(val), prior(rand()), l(NULL), r(NULL) { }
+    item (int val, int prior) : val(val), prior(prior), l(NULL), r(NULL) { }
+};
+typedef item* pitem;
+
+int cnt (pitem t) {
+    return t ? t->cnt : 0;
+}
+
+void upd_cnt (pitem t) {
+    if (t)
+        t->cnt = 1 + cnt(t->l) + cnt (t->r);
+}
+
+void merge (pitem & t, pitem l, pitem r) {
+    if (!l || !r)
+        t = l ? l : r;
+    else if (l->prior > r->prior)
+        merge (l->r, l->r, r),  t = l;
+    else
+        merge (r->l, l, r->l),  t = r;
+    upd_cnt (t);
+}
+
+void split (pitem t, pitem & l, pitem & r, int key, int add = 0) {
+    if (!t)
+        return void( l = r = 0 );
+    int cur_key = add + cnt(t->l); //implicit key
+    if (key <= cur_key)
+        split (t->l, l, t->l, key, add),  r = t;
+    else
+        split (t->r, t->r, r, key, add + 1 + cnt(t->l)),  l = t;
+    upd_cnt (t);
+}
+
 
